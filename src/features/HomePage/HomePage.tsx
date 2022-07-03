eef909345154cb7fba2965f9a406af4892e14583
Alexandr Axenov (axenov_it) <axenov.its@gmail.com>
2022-07-03 22:34:40 +0300

    Implement template generator

---
+++ src/features/HomePage/HomePage.tsx
@@ -0,0 +1,12 @@
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
+
+export const HomePage = () => (
+  <div
+    css={css`
+      font-size: 16px;
+    `}
+  >
+    <h1>HomePage component</h1>
+  </div>
+);
