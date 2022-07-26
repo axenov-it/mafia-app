71e6c1374ef76e5933e6f1057eb64615f93562ca
Alexandr Axenov (axenov_it) <axenov.its@gmail.com>
2022-07-26 20:08:35 +0300

    Add use scene hook

---
+++ src/common/hooks/useScene.ts
@@ -0,0 +1,17 @@
+/* eslint-disable no-restricted-globals */
+import { SceneTypes } from "common/interfaces";
+import { useDispatch, setScene } from "redux-store";
+
+export const useScene = () => {
+  const dispatch = useDispatch();
+
+  const runScene = (sceneType: SceneTypes, message: string) => {
+    const isRun = confirm(message);
+
+    if (isRun) {
+      dispatch(setScene(sceneType));
+    }
+  };
+
+  return { runScene };
+};
